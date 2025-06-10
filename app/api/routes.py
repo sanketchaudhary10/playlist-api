dbb79bcd0b4173cf2a566115acc3881f0ea8c854
sanketchaudhary10 <67291486+sanketchaudhary10@users.noreply.github.com>
2025-06-10 17:29:51 -0500

    Updated Code logic

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, Request
+from typing import Optional
+from fastapi import APIRouter, Request, Query
 
 router = APIRouter()
 
@@ -9,4 +10,32 @@ def root():
 @router.get("/songs")
 def get_all_songs(request: Request):
     return request.app.state.normalized_data
+
+@router.get("/songs/{title}")
+def get_all_songs(request: Request, title: str):
+    data = request.app.state.normalized_data
+    res = []
+
+    for song in data:
+        if song['title'].lower() == title.lower():
+            res.append(song)
+    
+    if res:
+        return res[0]
+
+    return {"Error": "Song not found! Please enter the correct name again."}
+
+
+# @router.get("/songs{title}")
+# def get_all_songs(request: Request, title: Optional[str] = Query(None, description="Title of the song")):
+#     data = request.app.state.normalized_data
+#     res = []
+
+#     if title:
+#         for song in data:
+#             if song['title'].lower() == title.lower():
+#                 res.append(song)
+#                 return res
+
+#     return data
 
